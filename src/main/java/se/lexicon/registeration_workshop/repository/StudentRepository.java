c0dee691c242fc4ec36f2b360645984cc026143e
aman adinlid <95pinkpanda@gmail.com>
2021-04-20 18:54:08 +0200

    ##Implmented all StudentRepository methods##

---
+++ src/main/java/se/lexicon/registeration_workshop/repository/StudentRepository.java
@@ -0,0 +1,18 @@
+package se.lexicon.registeration_workshop.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import se.lexicon.registeration_workshop.entity.Student;
+
+import java.util.List;
+
+public interface StudentRepository extends CrudRepository<Student, String> {
+
+    List<Student> findByEmail(String email);
+
+    List<Student> findByFirstNameAndLastName(String firstNme, String lastName);
+
+    List<Student> findByFirstName(String firstName);
+
+    List<Student> findByLastName(String lastName);
+
+}
