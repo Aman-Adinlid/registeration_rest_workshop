711865e3cbb55af76bfb6da28a4e46d8083d585b
aman adinlid <95pinkpanda@gmail.com>
2021-04-20 18:37:09 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/registeration_workshop/RegisterationWorkshopApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.registeration_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterationWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterationWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/registeration_workshop/RegisterationWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.registeration_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterationWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
